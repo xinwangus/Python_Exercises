c2841768eed9f2c7e74e0707fbc70d9b17d2cc39
Xin Wang <xinwangus@users.noreply.github.com>
2019-01-17 20:30:08 -0500

    Changes to be committed: 	new file:   regex_strip.py

---
+++ regex_strip.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import re
+
+def myStrip(s, chars=' '):
+    if (chars == ' '):
+        headp = re.compile(r"^\s*")
+        tailp = re.compile(r"\s*$")
+        return tailp.sub('', headp.sub('', s))
+    else:
+        head_re = r"^" + re.escape(chars)
+        tail_re = re.escape(chars) + r"$"
+        headp = re.compile(head_re)
+        tailp = re.compile(tail_re)
+        return tailp.sub('', headp.sub('', s))
+    
+s1 = "  asdsaf  fdsahfas  "
+print(s1.strip())
+print(myStrip(s1))
+
+s2 = "asdsaf  fdsahfas"
+print(s2.strip('as'))
+print(myStrip(s2, 'as'))
+        
+
