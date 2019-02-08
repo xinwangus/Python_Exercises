4160fddb87b385acddb2624476f75e95572831ec
Xin Wang <xinwangus@users.noreply.github.com>
2019-02-07 19:48:57 -0500

    Changes to be committed: 	new file:   wc_debug_in_Jupyter.py

---
+++ wc_debug_in_Jupyter.py
@@ -0,0 +1,36 @@
+'''
+A test python program similar to the unix wc command.
+    Show the debug capabilities inside Jupyter
+'''
+from IPython.core.debugger import set_trace
+'''
+or
+from pdb import set_trace
+'''
+
+def wc_fn(file):
+    try:
+        with open(file) as testfile:
+            linecount = 0
+            wordcount = 0
+            charcount = 0
+            for line in testfile:
+                linecount += 1
+                # count how many words each line
+                words = line.split()
+                wordcount += len(words)
+                for word in words:
+                    # different than wc, do not count space
+                    charcount += len(word)
+                
+        print('\t'+ str(linecount), \
+              str(wordcount), \
+              str(charcount), \
+              file, sep='\t')
+    except IOError:
+        print("File \"" + file + "\" not found!")
+
+# debug will stop here first
+set_trace()
+
+wc_fn("wc.py")
