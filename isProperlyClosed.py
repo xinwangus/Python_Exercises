284e0873cd4c6bd05acfa0ee37dc9dcffac9f58e
Xin Wang <xinwangus@users.noreply.github.com>
2019-02-07 23:26:34 -0500

    Changes to be committed: 	modified:   isProperlyClosed.py

--- isProperlyClosed.py
+++ isProperlyClosed.py
@@ -1,76 +1,45 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # A test program to check if a string containing
 # [] {} () are properly closed, and no interlace
 
 # Very similiar to the C++ version I wrote, but use
 # python list to simulate a "stack"
-
-
 def isClosed (s):
-        i = 0
+	i = 0
 	l = len(s)
-
-        nextToClose = [] # it is a list of two-member-list
-        # 1st member is the count of next to close
-        # 2nd member is the what is the next to close
-	# example: "{{[[[(" would give:
-        # [ [2, '}'] [3, ']'] [1, ')'] ]
+	nextToClose = []
     
 	while i < l:
-		if s[i] == '{':
-			if len(nextToClose) == 0 or nextToClose[-1][1] != '}':
-				nextToClose.append([1, '}'])
-			else:
-				nextToClose[-1][0] += 1
-		elif s[i] == '[':
-                        if len(nextToClose) == 0 or nextToClose[-1][1] != ']':
-                                nextToClose.append([1, ']'])
-                        else:   
-                                nextToClose[-1][0] += 1
-		elif s[i] == '(':
-                        if len(nextToClose) == 0 or nextToClose[-1][1] != ')':
-                                nextToClose.append([1, ')'])
-                        else:   
-                                nextToClose[-1][0] += 1
-		elif s[i] == '}':
-			if len(nextToClose) == 0 or nextToClose[-1][1] != '}':
-				return False
-                        else:
-				nextToClose[-1][0] -= 1
-				if nextToClose[-1][0] == 0:
-					nextToClose.pop()
+		if s[i] == '{' or s[i] == '(' or s[i] == '[' :
+			nextToClose.append(s[i])
 		elif s[i] == ']':
-			if len(nextToClose) == 0 or nextToClose[-1][1] != ']':
+			if (len(nextToClose) == 0) or (nextToClose[-1] != '['):
 				return False
-                        else:
-				nextToClose[-1][0] -= 1
-				if nextToClose[-1][0] == 0:
-					nextToClose.pop()
+			else:   
+				nextToClose.pop()
+		elif s[i] == '}':
+			if (len(nextToClose) == 0) or (nextToClose[-1] != '{'):
+				return False
+			else:   
+				nextToClose.pop()
 		elif s[i] == ')':
-			if len(nextToClose) == 0 or nextToClose[-1][1] != ')':
+			if (len(nextToClose) == 0) or (nextToClose[-1] != '('):
 				return False
-                        else:
-				nextToClose[-1][0] -= 1
-				if nextToClose[-1][0] == 0:
-					nextToClose.pop()
-		#print(nextToClose)
-		i = i + 1	
-
+			else:   
+				nextToClose.pop()		
+		i = i + 1
+		
 	if len(nextToClose) != 0:
 		return False
 	else:
 		return True
 
-
-while True:
-	print("String to check:")
-
-	# can just use  input() with python 3
-	ss = raw_input()
-
-	if isClosed(ss):
-		print (ss + " is Properly Closed, Good Job!")
-	else:
-		print (ss + " is not Properly Closed!")
+if __name__ == '__main__':
+	while True:
+		ss = input("String to check:")
+		if isClosed(ss):
+			print (ss + " is Properly Closed, Good Job!")
+		else:
+			print (ss + " is not Properly Closed!")
 		
