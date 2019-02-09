02c21f8c58193d242c3c2d3f55f3c03e2dacf897
Xin Wang <xinwangus@users.noreply.github.com>
2019-02-09 16:37:06 -0500

    Changes to be committed: 	modified:   regex_strip.py

--- regex_strip.py
+++ regex_strip.py
@@ -1,22 +1,12 @@
 #!/usr/bin/env python3
-import re
+from xwlibString import stripUsingRe
 
-def myStrip(s, chars=' '):
-    if (chars == ' '):
-        headp = re.compile(r"^\s*")
-        tailp = re.compile(r"\s*$")
-        return tailp.sub('', headp.sub('', s))
-    else:
-        head_re = r"^" + re.escape(chars)
-        tail_re = re.escape(chars) + r"$"
-        headp = re.compile(head_re)
-        tailp = re.compile(tail_re)
-        return tailp.sub('', headp.sub('', s))
-    
 if __name__ == '__main__':
     s1 = "  asdsaf  fdsahfas  "
     print(s1.strip())
-    print(myStrip(s1))
+    print(stripUsingRe(s1))
+    assert s1.strip() == stripUsingRe(s1)	
     s2 = "asdsaf  fdsahfas"
     print(s2.strip('as'))
-    print(myStrip(s2, 'as'))
+    print(stripUsingRe(s2, 'as'))
+    assert s2.strip('as') == stripUsingRe(s2, 'as')	
